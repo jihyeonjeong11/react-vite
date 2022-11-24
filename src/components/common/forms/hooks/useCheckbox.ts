16cad3e3dc2f58852e1db2ab8f6bab84a96e4730
ahreum.J <jar@ibstech.kr>
2022-11-24 09:27:05 +0900

    checkbox 정보 구조 array에서 object로 변경

--- src/components/common/forms/hooks/useCheckbox.ts
+++ src/components/common/forms/hooks/useCheckbox.ts
@@ -1,27 +1,47 @@
-import React, { MouseEventHandler, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fieldProps } from "../../../../types/Global";
 
-const useCheckbox = (array: Pick<fieldProps, "selectOption">["selectOption"]):[Array<string>, React.ChangeEventHandler, React.ChangeEventHandler] => {
-    // 선택된 항목 상태
-    const [checkedArr, setCheckedArr] = useState<Array<string>>([]);
+const useCheckbox = (
+    array: Pick<fieldProps, "selectOption">["selectOption"], 
+    defaultValue: Pick<fieldProps, "defaultValue">["defaultValue"]
+): [{[key: string]: boolean}, React.ChangeEventHandler, React.ChangeEventHandler] => {
+    // 전체 목록 선택 현황
+    const [result, setResult] = useState<{[key: string]: boolean}>({});
+    useEffect(() => {
+        let _result;
+        if (array.length > 0) {
+            array.forEach(item => _result[item.value] = false);
+            if(defaultValue) {
+                if(Array.isArray(defaultValue)) {
+                    defaultValue.forEach(item => _result[item] = true);
+                } else {
+                    _result[defaultValue] = true;
+                }
+            }
+            setResult(_result);
+        }
+    }, [array]);
     // 특정 상태 변경
     const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
         const targetValue = event.currentTarget.value;
-        const targetIndex = checkedArr.findIndex(item => item === targetValue);
-        if(targetIndex > 0) {
-            setCheckedArr(prev => prev.filter((item, index) => index !== targetIndex));
-        } else {
-            setCheckedArr(prev => [...prev, targetValue]);
-        }
+        setResult(prev => ({...prev, [targetValue]: !prev[targetValue]}));
     }
     // 전체 상태 변경
     const handleChangeAll = () => {
-        if(checkedArr.length === array.length) {
-            setCheckedArr([]);
+        const values = Object.values(result);
+        const _result = {...result};
+        if(values.every(val => val === true)) {
+            for (const property in result) {
+                _result[property] = false
+            }
         } else {
-            setCheckedArr(array.map(item => item.value));
+            for (const property in result) {
+                _result[property] = true
+            }
         }
-    };
-    return [checkedArr, handleChange, handleChangeAll];
+        setResult(_result);
+    }
+    return [result, handleChange, handleChangeAll];
 }
+
 export default useCheckbox;
