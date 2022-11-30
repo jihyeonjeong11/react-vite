7e854061d931c3b747f6a1ff521c2a6861456092
ahreum.J <jar@ibstech.kr>
2022-11-30 16:15:31 +0900

    메인시험 추가 모달 변경된 Form 컴포넌트 사용법에 맞춰 수정

--- src/components/common/dialog/components/MainExamDialog.tsx
+++ src/components/common/dialog/components/MainExamDialog.tsx
@@ -5,14 +5,12 @@ import { useIndexedDb } from "../../contexts/indexeddb";
 import Backdrop from "./Backdrop";
 
 import { useDialogsContextState } from "../../contexts/dialogs";
-import Form from "../../forms/form";
-import { FormItem } from "../../forms/formControl";
 import type { CommonForms } from "@/types/Global";
-import { addRegex, addRequired } from "../../forms/helpers";
 import { useToggle } from "../../hooks/useToggle";
 import LoadingSpinner from "./LoadingSpinner";
 import { createExam } from "../../temporal/examFakeClient";
 import ModalContainer from "./ModalContainer";
+import FormRoot from "../../forms/container/FormRoot";
 
 const dropIn = {
     hidden: {
@@ -48,7 +46,8 @@ const MainExamDialog = () => {
         // catch: 에러 이후 메인페이지로 강제이동
         const exam = await createExam(data.mec_nm);
         if (exam) {
-            alert("추가 성공");
+            // alert("추가 성공");
+            // 얼럿은 실패 케이스에 사용
             return turnOff();
         };
         
@@ -72,25 +71,19 @@ const MainExamDialog = () => {
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
                     >
-                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%_-_30rem)] h-[20%] border p-2">
+                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[calc(100%_-_30rem)] border p-2">
                             {loading ? (
                                 <div className="flex w-full h-full items-center justify-center flex-col">
                                     <LoadingSpinner />
                                     메인 시험을 등록 중입니다.
                                 </div>
                             ) : (
-                                <Form submit={submit}>
-                                    <FormItem
-                                        key="mec_nm"
-                                        dataKey="mec_nm"
-                                        name="메인시험명 입력"
-                                        type="desc"
-                                        applyOption={{
-                                            required: addRequired,
-                                        }}
-                                    />
-                                    <button type="submit">제출 버튼</button>
-                                </Form>
+                                <>
+                                <h4>메인 시험 추가</h4>
+                                <FormRoot submit={submit} type={"NewMainExam"}>
+                                    <button type="submit">추가</button>
+                                </FormRoot>
+                                </>
                             )}
                         </div>
                     </motion.div>
